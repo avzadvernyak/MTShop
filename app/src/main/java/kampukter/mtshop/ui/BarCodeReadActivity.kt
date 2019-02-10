7748b3e06712644a10d7dc7c1140dfbfb2c3e20a
Kampukter <a.v.zadvernyak@gmail.com>
2019-02-10 19:59:01 +0300

    Barr code 10/02/2019

--- app/src/main/java/kampukter/mtshop/ui/BarCodeReadActivity.kt
+++ app/src/main/java/kampukter/mtshop/ui/BarCodeReadActivity.kt
@@ -2,10 +2,8 @@ package kampukter.mtshop.ui
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.google.zxing.ResultPoint
@@ -17,18 +15,29 @@ import kotlinx.android.synthetic.main.bar_code_read.*
 
 class BarCodeReadActivity : AppCompatActivity() {
 
-    private var lastText : String? = null
+    private var lastText: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.bar_code_read)
 
+        btnTorchOn.setOnClickListener{zxing_barcode_scanner.setTorchOn()}
+        btnTorchOff.setOnClickListener { zxing_barcode_scanner.setTorchOff() }
+
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-            != PackageManager.PERMISSION_GRANTED) {
+            != PackageManager.PERMISSION_GRANTED
+        ) {
             Log.d("blablabla", " Permission is not granted")
+        } else {
+            zxing_barcode_scanner.decodeContinuous(callback)
+            zxing_barcode_scanner.resume()
+            //zxing_barcode_scanner.changeCameraParameters({_->setOrientationLocked()})
         }
+    }
 
-
+    override fun onDestroy() {
+        super.onDestroy()
+        zxing_barcode_scanner.pause()
     }
 
     private val callback = object : BarcodeCallback {
